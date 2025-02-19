89dbad914aa1a6cd2db01a8ce240f4cbc3c244c2
Kamindu-web <wildwebsolutionsl@gmail.com>
2025-02-19 09:40:57 +0530

    re-add autoplay

--- assets/kd-custom.js
+++ assets/kd-custom.js
@@ -7,6 +7,10 @@ var swiper = new Swiper(".kd-slider", {
     watchSlidesProgress: true,
     watchSlidesVisibility: true,
     centerInsufficientSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
     navigation: {
         nextEl: ".kd-slider-next-btn",
         prevEl: ".kd-slider-prev-btn",
@@ -24,41 +28,20 @@ var swiper = new Swiper(".kd-slider", {
             slidesPerView: 1.3,
             spaceBetween: 20
         }
-    },
-    on: {
-        init: function () {
-            setTimeout(() => {
-                this.update();
-                this.slideToLoop(0, 0);
-            }, 500);
-        },
-        resize: function () {
-            this.update();
-        },
-        slideChangeTransitionEnd: function () {
-            this.update();
-            fixSlideClasses();
-        }
     }
+    // ,
+    // on: {
+    //     init: function () {
+    //         setTimeout(() => {
+    //             this.update();
+    //             this.slideToLoop(0, 0);
+    //         }, 500);
+    //     },
+    //     resize: function () {
+    //         this.update();
+    //     },
+    //     slideChangeTransitionEnd: function () {
+    //         this.update();
+    //     }
+    // }
 });
-
-// Function to manually fix next & prev slide assignments
-function fixSlideClasses() {
-    setTimeout(() => {
-        let slides = document.querySelectorAll(".swiper-slide");
-        slides.forEach(slide => slide.classList.remove("swiper-slide-prev", "swiper-slide-next"));
-
-        let activeSlide = document.querySelector(".swiper-slide-active");
-        if (activeSlide) {
-            let prevSlide = activeSlide.previousElementSibling || slides[slides.length - 1];
-            let nextSlide = activeSlide.nextElementSibling || slides[0];
-
-            prevSlide.classList.add("swiper-slide-prev");
-            nextSlide.classList.add("swiper-slide-next");
-        }
-    }, 50);
-}
-
-// Ensure navigation buttons trigger `fixSlideClasses`
-document.querySelector(".kd-slider-next-btn").addEventListener("click", fixSlideClasses);
-document.querySelector(".kd-slider-prev-btn").addEventListener("click", fixSlideClasses);
