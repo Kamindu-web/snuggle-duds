4dbf9d17b7c606d712d8b82c458413a219f375f7
Kamindu-web <wildwebsolutionsl@gmail.com>
2025-02-19 09:37:26 +0530

    fixing slide classes

--- assets/kd-custom.js
+++ assets/kd-custom.js
@@ -37,6 +37,28 @@ var swiper = new Swiper(".kd-slider", {
         },
         slideChangeTransitionEnd: function () {
             this.update();
+            fixSlideClasses();
         }
     }
 });
+
+// Function to manually fix next & prev slide assignments
+function fixSlideClasses() {
+    setTimeout(() => {
+        let slides = document.querySelectorAll(".swiper-slide");
+        slides.forEach(slide => slide.classList.remove("swiper-slide-prev", "swiper-slide-next"));
+
+        let activeSlide = document.querySelector(".swiper-slide-active");
+        if (activeSlide) {
+            let prevSlide = activeSlide.previousElementSibling || slides[slides.length - 1];
+            let nextSlide = activeSlide.nextElementSibling || slides[0];
+
+            prevSlide.classList.add("swiper-slide-prev");
+            nextSlide.classList.add("swiper-slide-next");
+        }
+    }, 50);
+}
+
+// Ensure navigation buttons trigger `fixSlideClasses`
+document.querySelector(".kd-slider-next-btn").addEventListener("click", fixSlideClasses);
+document.querySelector(".kd-slider-prev-btn").addEventListener("click", fixSlideClasses);
