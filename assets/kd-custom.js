f9bdc286165125f9733ea3ff339e8f540a37e7f4
Kamindu-web <wildwebsolutionsl@gmail.com>
2025-02-21 10:49:03 +0530

    product slider

--- assets/kd-custom.js
+++ assets/kd-custom.js
@@ -78,7 +78,7 @@ document.addEventListener('DOMContentLoaded', function () {
     },
     breakpoints: {
       768: { slidesPerView: 2 },
-      1024: { slidesPerView: 4 },
+      1024: { slidesPerView: 6 },
     },
   });
 
